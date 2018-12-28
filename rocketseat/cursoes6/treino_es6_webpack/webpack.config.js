963d1baf38556a3c7647c7dea0f80a2e1704be7c
Joberth Rogers <joberth.rogers18@gmail.com>
2018-12-27 23:13:58 -0200

    aplication in css using api

---
+++ rocketseat/cursoes6/treino_es6_webpack/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: ['@babel/polyfill','./src/main.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '/public')
+  },
+  devServer:{
+    contentBase: __dirname + '/public'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/, // include .js files
+      exclude: /node_modules/, // exclude any and all files in the node_modules folder
+      use: [{
+        loader: "babel-loader",
+      }]
+    }]
+  },
+};

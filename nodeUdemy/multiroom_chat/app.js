c46620dd79f8fa971b863cdddfaf89da2a3b1e7c
Joberth Rogers <joberth.rogers18@gmail.com>
2019-01-11 19:52:07 -0300

    Using socket.io for establish the request between server and client in multichat Using funtion on and emit for make request

--- nodeUdemy/multiroom_chat/app.js
+++ nodeUdemy/multiroom_chat/app.js
@@ -1,8 +1,52 @@
 /* Import config server */
 var app = require('./config/server');
 
-
 //Listen port
-app.listen(3000, function(){
+var server = app.listen(3000, function(){
     console.log('Servidor is on!');
 });
+
+var io = require('socket.io').listen(server);// Ask for the socket io listen in the same port in the server http
+
+app.set('io', io); //the function set from express allow me to create variable with scope global and use this without pass by parameter
+
+/* create connection by websockt */ 
+
+io.on('connection', function(socket){
+    console.log('Usuario se conectou!');
+
+    socket.on('disconnect', function(){
+        console.log('Usuario se desconectou');
+    });
+
+    socket.on('msgParaServidor', function(data){
+        /* Dialog */
+        socket.emit( // take the message of cliente and pass to cliente again 
+            'msgParaCliente',
+             {apelido: data.apelido, mensagem: data.mensagem}
+        ); // the emit here  is for send the owner of message
+
+        socket.broadcast.emit( // the broadcast work send message for others in the same socket 
+            'msgParaCliente',
+             {apelido: data.apelido, mensagem: data.mensagem}
+        );
+        
+        /* People in call */
+        if(parseInt(data.apelido_atualizado_nos_cliente) == 0){
+            socket.emit( 
+                'participantesParaCliente',
+                {apelido: data.apelido}
+            ); 
+
+            socket.broadcast.emit(  
+                'participantesParaCliente',
+                {apelido: data.apelido, mensagem: data.mensagem}
+            );
+        }
+
+    });
+
+    
+}); // search for action in side of cliente, because we have an instance from io in ejs
+
+
--- nodeUdemy/multiroom_chat/app/controllers/chat.js
+++ nodeUdemy/multiroom_chat/app/controllers/chat.js
@@ -3,7 +3,7 @@ module.exports.initChat = function(application, req, res){
     var dataForm = req.body;
 
     req.assert('apelido', 'O campo não pode ser vazio!').notEmpty();
-    req.assert('apelido', 'O campo deve conter entre 3 e 15 caracteres!').len(3,5);
+    req.assert('apelido', 'O campo deve conter entre 3 e 15 caracteres!').len(3,15);
 
     var errors = req.validationErrors();
 
@@ -12,5 +12,11 @@ module.exports.initChat = function(application, req, res){
         return;
     }
 
-    res.render("chat", {errors: errors});
+    application.get('io').emit(
+        'msgParaCliente',
+        {apelido: dataForm.apelido, mensagem: ' acabou de entrar no chat'}
+    ); // take by set in app.js and I can recover this by application
+    //get() recover the variable io
+
+    res.render("chat", {dataForm: dataForm});
 }

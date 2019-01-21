1f1a93e13d4e758d456fc33f476977b502a3003c
Joberth Rogers <joberth.rogers18@gmail.com>
2019-01-21 20:01:35 -0300

    Sign in the posts in database and load in home page Using body parser

--- nodeUdemy/sistemaDeCadastro/app.js
+++ nodeUdemy/sistemaDeCadastro/app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
-const Sequelize = require('sequelize');
-const Op = Sequelize.Op;
+const Post = require('./models/Post');
+const db = require('./models/db');
+
 
 //Config
     //Template engine
@@ -15,20 +16,35 @@ const Op = Sequelize.Op;
     app.use(bodyParser.urlencoded({extended: false}));
     app.use(bodyParser.json());
 
-    //conexão com o banco de dados 
-    const sequelize = new Sequelize('test','root', 'winterfell', {
-        host: 'localhost',
-        dialect: 'mysql',
-        operatorsAliases: Op, // use Sequelize.Op
-    });
-
 //Rotas
     app.get('/cad', (req, res) => {
         res.render('formulario')
     });
 
+    app.get('/', function(req, res){
+        Post.findAll({
+            order: [
+                ['createdAt', 'DESC']
+            ]
+        }) // retorna tudo que estiver na tabela post
+        .then(function(posts){
+            res.render('home', {posts: posts})
+        })
+    })
+
     app.post('/add', (req, res)=> {
-        res.send('Texto: '+ req.body.titulo + ' Conteúdo: '+ req.body.conteudo);
+        Post.create({
+            titulo: req.body.titulo,
+            conteudo: req.body.conteudo
+        })
+        .then(function(){
+            res.redirect('/')
+        })
+        .catch(function(err){
+            res.send('Houve um erro : ' + err);
+        })
+        // posso usar Post.create(req.body); pois o que vem do formulario tem a mesma posicao e o nome das variaveis presentes no banco de dados
+        
     });
 
 

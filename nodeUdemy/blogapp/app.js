6cdc9068419faadb479d05a68569836edc242139
Joberth Rogers <joberth.rogers18@gmail.com>
2019-01-22 22:43:00 -0300

    Init blogapp and install dependeces new express.Router

---
+++ nodeUdemy/blogapp/app.js
@@ -0,0 +1,30 @@
+//Load modules
+const express = require("express");
+const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
+//const mongoose = require("mongoose");
+
+const admin = require('./routes/admin');
+
+const app = express();
+
+//Config
+    //Bodyparse
+        app.use(bodyParser.urlencoded({extended: true}));
+        app.use(bodyParser.json())
+
+    //Handlebars
+        app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+        app.set('view engine', 'handlebars');
+    //Mongoose  
+        //...
+
+//Routes
+    app.use('/admin', admin);
+//Others
+
+const PORT = 8081
+
+app.listen(PORT, () => {
+    console.log('Servidor is on!');
+});

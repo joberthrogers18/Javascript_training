8b3f11e1148b4f617efb040a0bde96aff11c6f07
Joberth Rogers <joberth.rogers18@gmail.com>
2019-01-23 08:51:21 -0300

    Connect to mongodb save category in database config session with express-session and flash use midlewares

--- nodeUdemy/blogapp/app.js
+++ nodeUdemy/blogapp/app.js
@@ -2,14 +2,31 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
 const bodyParser = require("body-parser");
-//const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const path = require("path");
+const session =  require('express-session');
+const flash = require("connect-flash");
 
 const admin = require('./routes/admin');
 
 const app = express();
 
 //Config
+    //Sessão
+        app.use(session({
+            secret: 'qualquer coisa///',
+            resave: true,
+            saveUninitialized: true
+        })) // creating session
+        app.use(flash())
+    
+    //Middleware
+        app.use((req, res, next) => {
+            res.locals.success_msg = req.flash("success_msg"); //variables global
+            res.locals.error_msg = req.flash("error_msg");
+            next();
+        })
+        
     //Bodyparse
         app.use(bodyParser.urlencoded({extended: true}));
         app.use(bodyParser.json())
@@ -17,8 +34,17 @@ const app = express();
     //Handlebars
         app.engine('handlebars', handlebars({defaultLayout: 'main'}));
         app.set('view engine', 'handlebars');
+
     //Mongoose  
-        //...
+        mongoose.Promise = global.Promise; // essential line, not forget to add here
+        mongoose.connect("mongodb://localhost/blogapp", {useNewUrlParser: true})
+        .then(() => {
+            console.log('Connect to mongo');
+        })
+        .catch(err => {
+            console.log('Error to connect mongo: '+ err);
+        });
+
     //Public
         app.use(express.static(path.join(__dirname, "public"))); // files from static files
 
--- nodeUdemy/blogapp/routes/admin.js
+++ nodeUdemy/blogapp/routes/admin.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router()
+const mongoose = require('mongoose');
+require('../models/Category');
+const Category = mongoose.model('category');
 
 router.get('/', (req, res) => {
     res.render("admin/index")
@@ -11,6 +14,18 @@ router.get('/posts', (req, res) => {
 
 router.get('/categorias/add', (req, res) => {
     res.render("admin/addcategorias");
+});
+
+router.post('/categorias/nova', (req, res) => {
+    var newCategory = req.body;
+
+    new Category(newCategory).save()
+    .then(() => {
+        console.log('Category was create successful');
+    })
+    .catch(err => {
+        console.log('Error to create new categorie: ' +  err);
+    }) // save in mongodb
 });
 
 router.get('/categorias', (req, res) => {

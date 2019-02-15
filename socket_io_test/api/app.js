0febd5d50eba936881ddfd5b373cbf53bb70f514
Joberth Rogers <joberth.rogers18@gmail.com>
2019-02-15 17:19:48 -0300

    Creating the front basic and using axios to make request with socket-io-client

---
+++ socket_io_test/api/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const axios = require("axios");
+const port = process.env.PORT || 4001;
+const index = require("./routes/index");
+const app = express();
+app.use(index);
+const server = http.createServer(app);
+const io = socketIo(server);
+io.on("connection", socket => {
+  console.log("New client connected"), setInterval(
+    () => getApiAndEmit(socket),
+    10000
+  );
+  socket.on("disconnect", () => console.log("Client disconnected"));
+});
+const getApiAndEmit = async socket => {
+  try {
+    const res = await axios.get(
+      "https://api.darksky.net/forecast/bbe3f6c1d6d19ab12cd359be7ea40599/37.8267,-122.4233"
+    );
+
+    console.log(res);
+
+    socket.emit("FromAPI", res.data.currently.temperature);
+  } catch (error) {
+    console.error(`Error: ${error.code}`);
+  }
+};
+server.listen(port, () => console.log(`Listening on port ${port}`));
+

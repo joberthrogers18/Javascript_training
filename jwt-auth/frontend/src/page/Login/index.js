192bad817c0a28b58f688ec0185c78259b8ae983
Joberth Rogers <joberth.rogers18@gmail.com>
2020-04-04 11:56:32 -0400

    Styling the login page
    Signed-off-by: Joberth Rogers <joberth.rogers18@gmail.com>

--- jwt-auth/frontend/src/page/Login/index.js
+++ jwt-auth/frontend/src/page/Login/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
-import Logo from '../../../assets/Logo.png';
+import Logo from '../../../assets/logo.png';
 
 function Login () {
   return (
     <View style={styles.container}>
       <Image source={Logo} />
+
+      <View style={styles.inputsContainer}>
+        <Text style={styles.inputLabel}>Email</Text>
+        <TextInput 
+          textContentType="emailAddress" 
+          style={styles.input} 
+          placeholder="Digite seu email aqui"
+          placeholderTextColor="#FFF"
+          />
+        <Text style={styles.inputLabel}>Senha</Text>
+        <TextInput 
+          textContentType="password" 
+          style={styles.input} 
+          placeholder="Digite sua senha"
+          placeholderTextColor="#FFF"
+        />
+      </View>
+      <TouchableOpacity style={styles.submitButton} >
+        <Text style={styles.textButton}>Login</Text>
+      </TouchableOpacity>
     </View>
   );
 }
--- jwt-auth/frontend/src/page/Login/styles.js
+++ jwt-auth/frontend/src/page/Login/styles.js
@@ -3,7 +3,55 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#bbe1fa'
+  },
+
+  inputsContainer: {
+    width: '100%',
+    height: 300,
+    backgroundColor: '#0f4c75',
+    borderWidth: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    padding: 10,
+    borderRadius: 15
+  },
+
+  input: {
+    height: 40,
+    width: '100%', 
+    borderColor: '#FFF',
+    borderWidth: 1,
+    marginBottom: 20,
+    borderRadius: 20,
+    paddingHorizontal: 10,
+    fontWeight: 'bold',
+  },
+  
+  inputLabel: {
+    fontWeight: 'bold',
+    fontSize: 15,
+    color: '#DDD',
+    marginLeft: 10,
+    marginBottom: 5
+  },
+
+  submitButton: {
+    width:  '100%',
+    height: 50,
+    backgroundColor: '#1b262c',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20
+  },
+
+  textButton: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    fontSize: 18,
   }
-})
+});

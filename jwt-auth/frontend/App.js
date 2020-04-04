bd6f4ae1a2ec6c5dd11d1b743d067ab9341f1b23
Joberth Rogers <joberth.rogers18@gmail.com>
2020-04-04 00:27:23 -0400

    Change the version from expo to work in emulator
    Signed-off-by: Joberth Rogers <joberth.rogers18@gmail.com>

--- jwt-auth/frontend/App.js
+++ jwt-auth/frontend/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
 import Routes from './src/routes';
 
@@ -8,12 +7,3 @@ export default function App() {
     <Routes />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
